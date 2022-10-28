b52e16c33f4afa71a56f38968cada8d6fa75a5e4
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-10-28 15:24:13 +0900

    URL 수정

--- common/Constants.tsx
+++ common/Constants.tsx
@@ -2,6 +2,6 @@ export const CLIENT_BASE_URL = 'https://beople.vercel.app/';
 export const API_SERVER_BASE_URL = 'http://beople.cf';
 
 export class AppRoutes {
-  static index = '/';
-  static login = '/login';
+  static index = `${CLIENT_BASE_URL}/`;
+  static login = `${CLIENT_BASE_URL}/login`;
 }
