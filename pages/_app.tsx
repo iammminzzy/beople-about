7a9c00b69ee361d4b4727780b6b0e60aea0466f1
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-10-28 15:22:46 +0900

    vercel 배포

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/beople.css';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>BEOPLE</title>
+        <link rel='icon' href='/Logo.svg' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
