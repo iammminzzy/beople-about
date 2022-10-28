d7b8289f4baff2c3719c6890486ed9e65761b51b
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-10-28 15:09:13 +0900

    about page 별도 프로젝트 생성

---
+++ common/Constants.tsx
@@ -0,0 +1,7 @@
+export const CLIENT_BASE_URL = 'http://localhost:3000';
+export const API_SERVER_BASE_URL = 'http://beople.cf';
+
+export class AppRoutes {
+  static index = '/';
+  static login = '/login';
+}
